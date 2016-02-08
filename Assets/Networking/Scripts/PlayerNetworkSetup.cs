aaab2f277f913e80167e0f260b6638ea4fe2c0b7
belkalah-mbp <belkacem.lahouel@gmail.com>
2016-02-07 22:16:34 -0500

    Adding networking treatment, rotation and position sync OK.

---
+++ Assets/Networking/Scripts/PlayerNetworkSetup.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerNetworkSetup : NetworkBehaviour {
+
+	public void Start() {
+		if (isLocalPlayer) {
+			GetComponent<Move>().enabled = true;
+			GetComponent<Rotate>().enabled = true;
+		}
+	}
+}
---
+++ Assets/Networking/Scripts/Rotate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	public float angularSpeed;
+
+	public void Update () {
+		float mouseX = Input.GetAxis("Mouse X");
+		//float mouseY = Input.GetAxis("Mouse Y");
+
+		//transform.Rotate(-mouseY * Time.deltaTime * angularSpeed, mouseX * Time.deltaTime * angularSpeed, 0f);
+		transform.Rotate(0f, mouseX * Time.deltaTime * angularSpeed, 0f);
+	}
+}
