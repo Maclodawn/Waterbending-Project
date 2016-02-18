510b3ddfe32c1475dbca75e33c9f4e2973b40f04
Julien Revel <julien.revel@telecomnancy.net>
2016-02-17 22:30:06 -0500

    WaterDeflect implementation (not finished)

--- Assets/Scripts/Fluids/WaterDeflect.cs
+++ Assets/Scripts/Fluids/WaterDeflect.cs
@@ -1,15 +1,47 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
-public class WaterDeflect : MonoBehaviour {
+public class WaterDeflect : MonoBehaviour
+{
+    public float m_avoidRadius, m_deflectRange, m_duration;
+    private float m_time;
+
+    private List<Drop> drops;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        m_time = m_duration;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, m_deflectRange);
+
+        foreach(Collider collider in colliders)
+        {
+            if(collider.tag == "Drop")
+            {
+
+            }
+        }
 	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Drop")
+        {
+            drops.Add(other.GetComponent<Drop>());
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Drop")
+        {
+            Drop drop = other.GetComponent<Drop>();
+            drops.Remove(drop);
+        }
+    }
 }
