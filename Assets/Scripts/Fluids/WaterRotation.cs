d6e3917bc112398fbf7851bf26d92a760d5f6a77
Julien Revel <julien.revel@telecomnancy.net>
2016-02-22 18:40:55 -0500

    Added the rotation effector

---
+++ Assets/Scripts/Fluids/WaterRotation.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterRotation : MonoBehaviour {
+
+    public float m_pullForce;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Drop drop = other.GetComponent<Drop>();
+        if (drop != null && drop.GetComponent<RotateEffector>() == null)
+        {
+            drop.removeEffectors();
+            RotateEffector target = drop.gameObject.AddComponent<RotateEffector>();
+            target.init(transform.position, Vector3.up, m_pullForce);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+
+    }
+}
