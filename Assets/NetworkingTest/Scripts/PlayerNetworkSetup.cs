e8499e1844a7df0da60d435a98d2989d0619e1b3
belkalah-mbp <belkacem.lahouel@gmail.com>
2016-03-04 16:28:45 -0500

    Player first attempt for networking

---
+++ Assets/NetworkingTest/Scripts/PlayerNetworkSetup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerNetworkSetup : NetworkBehaviour {
+
+	public void Start() {
+		if (isLocalPlayer) {
+			Move move = GetComponent<Move>();
+			if (move != null) move.enabled = true;
+
+			Rotate rotate = GetComponent<Rotate>();
+			if (rotate != null) rotate.enabled = true;
+
+			ComputeActionsFromInput computeActionsFromInput = GetComponent<ComputeActionsFromInput>();
+			if (computeActionsFromInput != null) computeActionsFromInput.enabled = true;
+		}
+	}
+}
