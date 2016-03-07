0e66a119a6ca06f5fce2f537015046acff1500ae
belkalah-mbp <belkacem.lahouel@gmail.com>
2016-03-06 20:26:18 -0500

    Parametrizing water prefabs for over-the-network syncing"

---
+++ Assets/Networking/Scripts/WaterNetworkSetup.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class WaterNetworkSetup : NetworkBehaviour {
+
+	private T enableScript<T>() where T : MonoBehaviour {
+		T t = GetComponent<T>();
+		if (t != null) t.enabled = true;
+		return t;
+	}
+
+	public void Start() {
+		if (isLocalPlayer) {
+			enableScript<Drop>();
+			enableScript<WaterReserve>();
+			enableScript<WaterGroup>();
+		}
+	}
+}
+
