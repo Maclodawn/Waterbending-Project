f7a0d33269e06bc25979df0a3b9aa9d4cad6cbf3
Julien Revel <Julien Revel>
2016-03-19 22:12:28 -0400

    Linked options to the menu

--- Assets/Scripts/Menus/PauseMenu.cs
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
     public GameObject mainMenu, optionsMenu;
+    public Slider sensitivitySlider;
+    public Toggle reverseYToggle;
 
     public void Resume()
     {
@@ -20,5 +23,15 @@ public class PauseMenu : MonoBehaviour
     {
         mainMenu.SetActive(true);
         optionsMenu.SetActive(false);
+    }
+
+    public void SetSensitivity()
+    {
+        Manager.getManager().m_cameraSpeed = sensitivitySlider.value;
+    }
+
+    public void SetYCamDir()
+    {
+        Manager.getManager().m_yReversed = reverseYToggle.isOn;
     }
 }
