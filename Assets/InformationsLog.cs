4b4026f1aa67aa6a144090913b03e3f28cd0030f
belkalah-mbp <belkacem.lahouel@gmail.com>
2016-04-18 14:34:27 -0400

    Text in the up-right corner containing information

---
+++ Assets/InformationsLog.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InformationsLog : MonoBehaviour {
+
+	private GUIText text = null;
+
+	public void Start() {
+		text = GetComponent<GUIText>();
+	}
+
+	public void Update() {
+		
+	}
+
+	public void OnGUI() {
+		text.pixelOffset = new Vector2(Screen.width-10, Screen.height-10);
+	}
+}
