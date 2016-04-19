2765b8f0da1db4c49c1df4a6bc3183e26eef09a9
belkalah-mbp <belkacem.lahouel@gmail.com>
2016-04-19 12:12:57 -0400

    Bars facing camera now, controlled with a boolean in editor

--- Assets/InGameHealthBarController.cs
+++ Assets/InGameHealthBarController.cs
@@ -4,14 +4,22 @@ using UnityEngine.UI;
 
 public class InGameHealthBarController : MonoBehaviour {
 
-	//on screen display of health
-	public Image health_img;
+	//main camera on scene: should be set if we want health bars to face camera
+	public bool face_camera = true;
+	private Camera main_camera = null;
+
+	//on screen display of health: health_bar contains the whole bar and health_img only the life bar
+	public Image health_img = null;
 
 	//health component managing health of player
 	public HealthComponent health;
 
+	public void Start() {
+		
+	}
+
 	public void OnGUI() {
-		//checking if component are properly set by user
+		//checking if components are properly set by user
 		if (health_img == null) {
 			Debug.LogError("Health image component not found.");
 			return;
@@ -26,5 +34,19 @@ public class InGameHealthBarController : MonoBehaviour {
 		health_img.transform.localScale = new Vector3(health.Health/health.MaxHealth,
 														health_img.transform.localScale.y,
 														health_img.transform.localScale.z);
+
+		//updating orientation towards camera
+		if (main_camera != null) {
+			//checking if the three canvas is referenced
+			if (gameObject == null) {
+				Debug.LogError("Health bar object not found.");
+				return;
+			}
+
+			gameObject.transform.LookAt(new Vector3(main_camera.transform.position.x,
+													main_camera.transform.position.y,
+													main_camera.transform.position.z));
+		} else if (face_camera)
+			main_camera = GameObject.FindObjectOfType<Camera>();
 	}
 }
