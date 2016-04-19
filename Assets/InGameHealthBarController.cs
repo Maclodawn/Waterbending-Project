8a0d3c20d2242dc010a909b8f758afb00f482b4d
belkalah-mbp <belkacem.lahouel@gmail.com>
2016-04-19 10:30:02 -0400

    Health bar integrated in game

---
+++ Assets/InGameHealthBarController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InGameHealthBarController : MonoBehaviour {
+
+	//on screen display of health
+	public Image health_img;
+
+	//health component managing health of player
+	public HealthComponent health;
+
+	public void OnGUI() {
+		//checking if component are properly set by user
+		if (health_img == null) {
+			Debug.LogError("Health image component not found.");
+			return;
+		}
+
+		if (health == null) {
+			Debug.LogError("Health component not found.");
+			return;
+		}
+
+		//update of health image from health component
+		health_img.transform.localScale = new Vector3(health.Health/health.MaxHealth,
+														health_img.transform.localScale.y,
+														health_img.transform.localScale.z);
+	}
+}
