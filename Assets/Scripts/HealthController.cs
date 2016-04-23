b63166ebe50781d80e746d040c87fe634a636e92
Gabriel <gabriel.rassoul@gmail.com>
2016-04-23 16:35:30 -0400

    Health syncronization done
    - Fix: Two HealthComponent on the player prefab instead of one
    - Rename each player's game object with their netId
    - Weird error in SelectingWaterState null character or waterReserve
    -

--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -3,38 +3,44 @@ using System;
 using System.Collections;
 using UnityEngine.Networking;
 
-public class HealthController : NetworkBehaviour {
+public class HealthController : NetworkBehaviour
+{
 
-	private HealthComponent health = null;
-	private InformationsLog informations = null;
+    private HealthComponent health = null;
+    private InformationsLog informations = null;
 
-	public void Start() {
-		//retrieving health component
-		health = gameObject.GetComponent<HealthComponent>();
-		if (health == null)
-			throw new ArgumentException("Health Component not found.");
+    public void Start()
+    {
+        //retrieving health component
+        health = gameObject.GetComponent<HealthComponent>();
+        if (health == null)
+            throw new ArgumentException("Health Component not found.");
 
-		//retrieving InformationsLog
-		informations = GameObject.Find("InformationsLog").GetComponent<InformationsLog>();
-	}
+        //retrieving InformationsLog
+        informations = GameObject.Find("InformationsLog").GetComponent<InformationsLog>();
+    }
 
-	[ServerCallback]
-	public void OnTriggerEnter(Collider collider) {
-		applyDamage(collider);
-	}
+    [ServerCallback]
+    public void OnTriggerEnter(Collider collider)
+    {
+        applyDamage(collider);
+    }
 
-	//when collision, apply damages to player's health component
-	[Server]
-	public void applyDamage(Collider collider) {
-		if (collider.gameObject.tag.Contains("Drop")) {
-			int tmp_dmg = UnityEngine.Random.Range(10, 15);
-			health.Health -= tmp_dmg; //TODO way of computing damage=f(power)?
-			informations.log("<b><color=\"blue\">" + gameObject.name + "</color></b>: -" + tmp_dmg + "PV");
-			if (health.Health < 1) {
-				//you're dead if your current player is dead
-				informations.log("<b><color=\"red\">" + gameObject.name + "</color></b> IS DEAD");
-				NetworkServer.Destroy(gameObject);
-			}
-		}
-	}
+    //when collision, apply damages to player's health component
+    [Server]
+    public void applyDamage(Collider collider)
+    {
+        if (collider.gameObject.tag.Contains("Drop"))
+        {
+            int tmp_dmg = UnityEngine.Random.Range(10, 15);
+            health.Health -= tmp_dmg; //TODO way of computing damage=f(power)?
+            informations.log("<b><color=\"blue\">" + gameObject.name + "</color></b>: -" + tmp_dmg + "PV");
+            if (health.Health < 1)
+            {
+                //you're dead if your current player is dead
+                informations.log("<b><color=\"red\">" + gameObject.name + "</color></b> IS DEAD");
+                NetworkServer.Destroy(gameObject);
+            }
+        }
+    }
 }
