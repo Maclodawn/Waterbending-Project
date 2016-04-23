11ca9e06974e1a3cb2515e679718ca6b5c81bb19
belkalah-mbp <belkacem.lahouel@gmail.com>
2016-04-23 17:17:20 -0400

    Using Double Bar for everybody + Power update

--- Assets/InGameDoubleBarController.cs
+++ Assets/InGameDoubleBarController.cs
@@ -10,29 +10,33 @@ public class InGameDoubleBarController : MonoBehaviour {
 
 	//on screen display of health: health_bar contains the whole bar and health_img only the life bar
 	public Image health_img = null;
+	public Image power_img = null;
 
 	//health component managing health of player
-	public HealthComponent health;
+	public HealthComponent health_component;
+	public PowerComponent power_component;
 
 	public void Start() {
 
 	}
 
 	public void OnGUI() {
-		//life update
+		//
+		//health update
+		//
 		//checking if components are properly set by user
 		if (health_img == null) {
 			Debug.LogError("Health image component not found.");
 			return;
 		}
 
-		if (health == null) {
+		if (health_component == null) {
 			Debug.LogError("Health component not found.");
 			return;
 		}
 
 		//update of health image from health component
-		health_img.transform.localScale = new Vector3(health.Health/health.MaxHealth,
+		health_img.transform.localScale = new Vector3(health_component.Health/health_component.MaxHealth,
 			health_img.transform.localScale.y,
 			health_img.transform.localScale.z);
 
@@ -50,6 +54,23 @@ public class InGameDoubleBarController : MonoBehaviour {
 		} else if (face_camera)
 			main_camera = GameObject.FindObjectOfType<Camera>();
 
-		//power update: TODO
+		//
+		//power update
+		//
+		//checking if components are properly set by user
+		if (power_img == null) {
+			Debug.LogError("Power image component not found.");
+			return;
+		}
+
+		if (power_component == null) {
+			Debug.LogError("Power component not found.");
+			return;
+		}
+
+		//update of health image from health component
+		power_img.transform.localScale = new Vector3(power_component.Power/power_component.MaxPower,
+			power_img.transform.localScale.y,
+			power_img.transform.localScale.z);
 	}
 }
