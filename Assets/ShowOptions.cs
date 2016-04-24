5002f5f0ac182ccee75099bcc2148945a9be31c2
belkalah-mbp <belkacem.lahouel@gmail.com>
2016-04-24 17:59:06 -0400

    Adding options menu + integration. Warning: options unaccessible, we need Manager.Instance()

---
+++ Assets/ShowOptions.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ShowOptions : MonoBehaviour {
+
+	public GameObject options;
+	public GameObject main_buttons;
+	public Slider sensitivitySlider;
+	public Toggle reverseYToggle;
+
+	public void showOptions() {
+		main_buttons.SetActive(false);
+		options.SetActive(true);
+	}
+
+	public void showMainButtons() {
+		main_buttons.SetActive(true);
+		options.SetActive(false);
+	}
+
+	public void SetSensitivity() {
+		Manager.getInstance().m_cameraSpeed = sensitivitySlider.value;
+	}
+
+	public void SetYCamDir()
+	{
+		Manager.getInstance().m_yReversed = reverseYToggle.isOn;
+	}
+}
