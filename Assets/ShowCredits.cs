0b0effd0ebef0efd8e2f62af27a57c2203474e9e
belkalah-mbp <belkacem.lahouel@gmail.com>
2016-04-24 17:14:22 -0400

    Adding credits + integration

---
+++ Assets/ShowCredits.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ShowCredits : MonoBehaviour {
+
+	public GameObject credits;
+	public GameObject main_buttons;
+
+	public void showCredits() {
+		main_buttons.SetActive(false);
+		credits.SetActive(true);
+	}
+
+	public void showMainButtons() {
+		main_buttons.SetActive(true);
+		credits.SetActive(false);
+	}
+}
