571f197e9fd59b73969eeaccdfcaead4c937858c
belkalah-mbp <belkacem.lahouel@gmail.com>
2016-04-24 21:37:02 -0400

    Bug correction: fake players no longer removed but disabled

--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -41,7 +41,7 @@ public class HealthController : NetworkBehaviour
                 //you're dead if your current player is dead
                 informations.log("<b><color=\"red\">" + gameObject.name + "</color></b> IS DEAD");
 				if (GetComponent<FakePlayer>())
-					NetworkIdentity.Destroy(gameObject);
+					gameObject.SetActive(false); //NetworkIdentity.Destroy(gameObject);
             }
         }
     }
